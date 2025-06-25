9ed9c8a4dc6a4026defb195eadc01fb278aa1342
akannika <a.kannika80@gmail.com>
2025-06-25 16:27:04 -0700

    Chapter 0 Introduction to Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// the first program 
+// using: rustc src/main.rs to compile main.rs --> get main.exe
+// call: ./main 
+// using cargo package manager to run the file --> cargo run
 fn main() {
     println!("Hello, world!");
 }
